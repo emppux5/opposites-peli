f909da0fd8f36d2c10e8e7114837c2b52bb0b0cc
moiasdkj <officialdeluxo@gmail.com>
2026-03-18 10:49:48 +0200

    Update page.tsx

--- opposites-peli/app/game/page.tsx
+++ opposites-peli/app/game/page.tsx
@@ -99,10 +99,10 @@ const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
   }
 
   return (
-    <div style={{ display: 'flex', gap: '40px', paddingTop: '202px', padding: '20px' , justifyContent: 'center'}}>
+    <div style={{ display: 'flex', gap: '40px', padding: '20px' , justifyContent: 'center'}}>
       
       {/* PLAYER UI */}
-      <div style={{ maxWidth: '400px' }}>
+      <div style={{ maxWidth: '400px', paddingTop: '25%' }}>
         <h1>Sinä</h1>
 
         <div style={{ marginBottom: '1rem', fontSize: '1.2rem' }}>
@@ -142,7 +142,7 @@ const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
       </div>
 
       {/* BOT UI */}
-      <div style={{ maxWidth: '400px' }}>
+      <div style={{ maxWidth: '400px', paddingTop: '25%' }}>
         <h1>Botti</h1>
 
         <div style={{ marginBottom: '1rem', fontSize: '1.2rem' }}>
